ad9460a88c75a33af6eca9f4a180e16e3df1b7ec
Juan Fangano <105882155+JuanyFanga@users.noreply.github.com>
2024-10-23 11:38:10 -0300

    New SpawnPosition system

--- Sigil IA Project/Assets/Scripts/Player/PlayerSpawn.cs
+++ Sigil IA Project/Assets/Scripts/Player/PlayerSpawn.cs
@@ -4,17 +4,33 @@ using UnityEngine;
 
 public class PlayerSpawn : MonoBehaviour
 {
-    [SerializeField] Vector2 spawnZoneX;
-    [SerializeField] Vector2 spawnZoneZ;
+    [SerializeField] Transform[] spawnPositions;
+
+    float Xvariation = 2f;
+    float Zvariation = 2f;
 
     void Start()
     {
-        transform.position = new Vector3(Random.Range(spawnZoneX.x, spawnZoneX.y), 0, Random.Range(spawnZoneZ.x, spawnZoneZ.y));
+        RandomSpawn();
     }
 
-    private void OnDrawGizmos()
+    private void RandomSpawn()
     {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireCube(transform.position, new Vector3 (6,0,3));
+        if (spawnPositions.Length > 0)
+        {
+            int randomIndex = Random.Range(0, spawnPositions.Length);
+            Transform newSpawnPosition = spawnPositions[randomIndex];
+
+            float newXvariation = Random.Range(-Xvariation, Xvariation);
+            float newZvariation = Random.Range(-Zvariation, Zvariation);
+
+            Debug.Log($"New variation in X is: {newXvariation}");
+            Debug.Log($"New variation in Z is: {newZvariation}");
+
+            transform.position = new Vector3(
+                newSpawnPosition.position.x + newXvariation,
+                0,
+                newSpawnPosition.position.z + newZvariation);
+        }
     }
 }
